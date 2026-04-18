f8b4e0a50aac968d03101da71b33dbcce1115dd8
Markito333 <markitodiazfaulin@gmail.com>
2026-04-18 12:32:03 -0400

    add the modular js

---
+++ data/technologies.js
@@ -0,0 +1,111 @@
+const technologies = [
+  {
+    name: 'Java',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg',
+    alt: 'Java'
+  },
+  {
+    name: 'JavaScript',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+    alt: 'JavaScript'
+  },
+  {
+    name: 'HTML',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
+    alt: 'HTML'
+  },
+  {
+    name: 'CSS',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg',
+    alt: 'CSS'
+  },
+  {
+    name: 'Node',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg',
+    alt: 'Node.js'
+  },
+  {
+    name: 'Spring',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg',
+    alt: 'Spring Boot'
+  },
+  {
+    name: 'PostgreSQL',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg',
+    alt: 'PostgreSQL'
+  },
+  {
+    name: 'Postman',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postman/postman-original.svg',
+    alt: 'Postman'
+  },
+  {
+    name: 'PHP',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg',
+    alt: 'PHP'
+  },
+  {
+    name: 'Bulma',
+    icon: null,
+    alt: 'Bulma',
+    isSvg: true,
+    svgContent: '<svg width="64" height="64" viewBox="0 0 220 320" alt="Bulma"><polygon fill="#00D1B2" points="0 220 20 80 100 0 200 100 140 160 220 240 100 320"></polygon></svg>'
+  },
+  {
+    name: 'VS Code',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg',
+    alt: 'VS Code'
+  },
+  {
+    name: 'WP',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/wordpress/wordpress-original.svg',
+    alt: 'WordPress'
+  },
+  {
+    name: 'Docker',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg',
+    alt: 'Docker'
+  },
+  {
+    name: 'Python',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg',
+    alt: 'Python'
+  },
+  {
+    name: 'Vite',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vitejs/vitejs-original.svg',
+    alt: 'Vite'
+  },
+  {
+    name: 'React',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+    alt: 'React'
+  },
+  {
+    name: 'Tailwind',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg',
+    alt: 'TailwindCSS'
+  },
+  {
+    name: 'Git',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg',
+    alt: 'Git'
+  },
+  {
+    name: 'GitHub',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg',
+    alt: 'GitHub'
+  },
+  {
+    name: 'NextJS',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg',
+    alt: 'Next.js'
+  },
+  {
+    name: 'Typescript',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
+    alt: 'TypeScript'
+  }
+];
+
+export default technologies;
